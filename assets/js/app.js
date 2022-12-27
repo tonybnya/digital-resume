3ad12bb54679e3fa2992de75006ddc8cf77fa5e6
Tony <nya.tony2010@gmail.com>
2022-12-27 19:20:19 +0100

    Update

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,34 @@
+gsap.registerPlugin(ScrollTrigger);
 let tl = gsap.timeline();
 
-tl.from(".picture", { duration: 5, opacity: 0, scale: .3, ease: "back" });
-tl.to(".picture", { duration: 5, rotation: 360, ease: "back" });
+tl.from(
+  ".picture",
+  { duration: 5, opacity: 0, scale: .3, ease: "back" }
+);
 
-gsap.to(".icon", { duration: 10, rotation: 360 , backgroundColor: "#B8A17A", borderRadius: "50%", padding: "5px" });
+tl.to(
+  ".picture",
+  { duration: 5, rotation: 360, ease: "back" }
+);
 
-gsap.to(".pointer", { duration: 10, rotation: 360 , backgroundColor: "#FFFFFF", borderRadius: "50%", padding: "3px" });
+gsap.to(".icon", {
+  scrollTrigger: {
+    trigger: ".icon",
+    toggleActions: "restart none none none"
+  },
+  duration: 10,
+  rotation: 360,
+  backgroundColor: "#B8A17A",
+  borderRadius: "50%",
+  padding: "5px"
+});
 
-gsap.to(".joinme, .fa-file, .fa-brands", { duration: 10, rotation: 360 });
+gsap.to(
+  ".pointer",
+  { duration: 10, rotation: 360, backgroundColor: "#FFFFFF", borderRadius: "50%", padding: "3px" }
+);
+
+gsap.to(
+  ".joinme, .fa-file, .fa-brands",
+  { duration: 10, rotation: 360 }
+);
